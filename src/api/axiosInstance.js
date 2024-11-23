cf42c6ea9b06de4b382e4d0c911b067bd4417166
Eddy Sejas Arze <eddy.sejas.arze.03@gmail.com>
2024-11-23 14:13:48 -0400

    Update axiosInstance.js

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-  baseURL: "https://audiobooks-66oq.onrender.com/api",
+  baseURL: "https://audiobooks-backend-production.up.railway.app/api",
   timeout: 5000,
 })
 
