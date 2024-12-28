16da8dc5d4adb70098a525a83fbcf161298d0323
Eddy Sejas Arze <eddy.sejas.arze.03@gmail.com>
2024-12-28 12:51:51 -0400

    modified types

---
+++ src/app/core/models/api-response.interface.ts
@@ -0,0 +1,40 @@
+export interface IApiResponse<T> {
+  executionTime: string
+  count: number
+  content: T[]
+}
+
+export interface IContent {
+  id: number
+  title: string
+  author?: string
+  duration: string
+  description: string
+  cover: string
+  link: string
+}
+
+export interface IContentSaved extends IContent {
+  progress?: string
+  saved_at?: string
+}
+
+export interface ILoginResponse {
+  token: string
+  user: IUser
+}
+
+export interface IUser {
+  id: number
+  name?: string
+  lastname?: string
+  username?: string
+  email: string
+  created_at?: string
+}
+
+export interface IGenre {
+  id: number
+  title: string
+  description?: string
+}
