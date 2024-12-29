32e8946586176f31a936d6df803092d7eb4797de
Eddy Sejas Arze <eddy.sejas.arze.03@gmail.com>
2024-12-29 10:27:13 -0400

    restructured icons and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { NavigationComponent } from './shared/components/navigation/navigation.component';
-
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavigationComponent],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet],
+  templateUrl: 'app.component.html',
 })
 export class AppComponent { }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import "bootstrap-icons/font/bootstrap-icons.min.css"
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
