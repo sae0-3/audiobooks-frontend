16d457f6016d0a148dacbdda7d9a8a4a96b55f61
Eddy Sejas Arze <eddy.sejas.arze.03@gmail.com>
2024-12-28 21:54:56 -0400

    pages added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './features/home/home.component';
+import { LibraryComponent } from './features/library/library.component';
+import { LoginComponent } from './features/login/login.component';
+import { PlayerComponent } from './features/player/player.component';
+import { RegisterComponent } from './features/register/register.component';
+import { SearchComponent } from './features/search/search.component';
+
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'library', component: LibraryComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'player/:id', component: PlayerComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'search', component: SearchComponent },
+];
---
+++ src/app/features/player/player.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-player',
+  imports: [],
+  template: `
+    <p>
+      player works!
+    </p>
+  `,
+  styles: ``
+})
+export class PlayerComponent {
+
+}
